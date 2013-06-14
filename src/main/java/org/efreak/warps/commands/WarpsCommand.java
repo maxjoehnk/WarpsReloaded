bb5abfc857ccfc82e727720a7fbd7922672f98b2
e-freak <maxjoehnk@gmail.com>
2013-06-14 14:22:18 +0200

    added permissions query

--- src/main/java/org/efreak/warps/commands/WarpsCommand.java
+++ src/main/java/org/efreak/warps/commands/WarpsCommand.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.efreak.warps.IOManager;
+import org.efreak.warps.Permissions;
 import org.efreak.warps.Warp;
 import org.efreak.warps.WarpsReloaded;
 import org.efreak.warps.help.HelpManager;
@@ -32,17 +33,18 @@ public class WarpsCommand implements CommandExecutor {
 		if (sender instanceof Player) p = (Player) sender;
 		if (sender instanceof ConsoleCommandSender) c = (ConsoleCommandSender) sender;
 		if (p == c) return false;
-		if (args.length == 0) return false;
 		else {
 			if (label.equalsIgnoreCase("warps")) {
 				if (args.length > 1) return false;
-				int pageCount = 1;
-				List<Warp> warps = WarpsReloaded.getWarpList();
-				if (args.length == 1) {
-					
-				}else {
-					io.sendHeader(sender, "WARPS (1/" + pageCount + ")");
-					for (int i = 0; i < warps.size() && i < 9; i++) io.send(sender, warps.get(i).getName(), false);
+				if (Permissions.has(sender, "warps.list", false, "/warps")) {
+					int pageCount = 1;
+					List<Warp> warps = WarpsReloaded.getWarpList();
+					if (args.length == 1) {
+						
+					}else {
+						io.sendHeader(sender, "WARPS (1/" + pageCount + ")");
+						for (int i = 0; i < warps.size() && i < 9; i++) io.send(sender, warps.get(i).getName(), false);
+					}
 				}
 				return true;
 			}else return false;
